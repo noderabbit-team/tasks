a0cb1578742cf0034ecb4ab2f965e95c60622e8a
Nate Aune <natea@jazkarta.com>
2011-02-06 13:16:33 -0500

    Configure Celery to talk to RabbitMQ server running on web node.

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,7 +1,7 @@
-BROKER_HOST = "localhost"
+BROKER_HOST = "ec2-72-44-42-23.compute-1.amazonaws.com"
 BROKER_PORT = 5672
-BROKER_USER = "myuser"
-BROKER_PASSWORD = "mypassword"
+BROKER_USER = "nrweb"
+BROKER_PASSWORD = "MiXNYPwsKbRkQCEU"
 
 CELERY_RESULT_BACKEND = "amqp"
 CELERY_IMPORTS = ("tasks", )
