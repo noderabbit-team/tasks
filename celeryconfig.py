44c2632857d386db5bf618abfdd73b8d1d414443
Kapil Thangavelu <kapilt@gmail.com>
2011-02-06 16:52:28 -0500

    sample celery config.

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,7 +1,10 @@
 BROKER_HOST = "localhost"
 BROKER_PORT = 5672
-BROKER_USER = "myuser"
-BROKER_PASSWORD = "mypassword"
+BROKER_USER = "djz"
+BROKER_PASSWORD = "djz"
+BROKER_VHOST = "djz"
 
-CELERY_RESULT_BACKEND = "amqp"
-CELERY_IMPORTS = ("tasks", )
+CELERY_RESULT_BACKEND = "database"
+CELERY_RESULT_DBURI = "postgresql://nrweb:nrweb@/nrweb"
+
+CELERY_IMPORTS = ("dz.tasks", )
