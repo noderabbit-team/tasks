0a0ed04d184af9edc2764bf7534fb203993d7b1a
Shimon Rura <shimon@rura.org>
2011-02-24 14:18:16 -0500

    change back to the official NR_BUNDLE_BUCKET setting

--- dz/tasklib/taskconfig.py
+++ dz/tasklib/taskconfig.py
@@ -1,7 +1,7 @@
 NR_PIP_REQUIREMENTS_FILENAME = 'noderabbit_requirements.txt'
 NR_PTH_FILENAME = 'noderabbit.pth'
 
-NR_BUNDLE_BUCKET = "nr-bundles"
+NR_BUNDLE_BUCKET = "nr-bundle-bucket"
 
 NR_CUSTOMER_DIR = "/cust"
 
