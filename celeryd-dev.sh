310b0b1bc05bf520cb80924024c29d855876d813
Shimon Rura <shimon@rura.org>
2011-03-06 15:29:39 -0500

    adding frontend_proxy queue

--- celeryd-dev.sh
+++ celeryd-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export CELERY_CONFIG_MODULE=celeryconfig_debug
-exec celeryd --concurrency=2 -Q build,appserver,database,celery,appserver:localhost -l debug
+exec celeryd --concurrency=2 -Q build,appserver,database,celery,appserver:localhost,frontend_proxy -l debug
