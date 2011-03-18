b2256466548a532e0a609074d40367e4c60d1c3f
Shimon Rura <shimon@rura.org>
2011-03-18 18:25:51 -0400

    warn politely about any running celery instances before trying to run all tests.

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+if (pgrep celery > /dev/null); then
+    echo It appears a celery process is running. You should kill it before attempting to run the test suite.
+    exit 1
+fi
+
 export CELERY_CONFIG_MODULE=celeryconfig_testing 
 exec nosetests -vv
