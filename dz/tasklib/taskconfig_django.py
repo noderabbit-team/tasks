543af49db0c1fe9969e1f2a70bb3f7e0460c350e
Shimon Rura <shimon@rura.org>
2011-03-25 18:26:15 -0400

    Added django 1.3

--- dz/tasklib/taskconfig_django.py
+++ dz/tasklib/taskconfig_django.py
@@ -2,9 +2,18 @@ DJANGO_VERSIONS = {
     "1.2": dict(caption="Django-1.2 latest (currently 1.2.5)",
                 pip_line="Django==1.2.5",
                 tarball="Django-1.2.5.tar.gz"),
+    "1.3": dict(caption="Django-1.3 latest (currently 1.3)",
+                pip_line="Django==1.3",
+                tarball="Django-1.3.tar.gz"),
 }
-DJANGO_VERSION_DEFAULT = "1.2"
+DJANGO_VERSION_DEFAULT = "1.3"
 
-DJANGO_VERSIONS_CHOICES = [ (k, v["caption"]) for (k,v) in DJANGO_VERSIONS.items() ]
+DJANGO_VERSIONS_CHOICES = [(k, v["caption"]) for (k, v) in
+                           DJANGO_VERSIONS.items()]
+# sort with latest version first
+DJANGO_VERSIONS_CHOICES.sort(key=lambda (k, v): k, reverse=True)
 
 DJANGO_TARBALLS_DIR = "/cust/django"
+
+# Get your tarballs!
+# wget http://www.djangoproject.com/download/1.3/tarball/
