88714067c2b5b9ab4e592b6fe3b2855136d75aa4
Shimon Rura <shimon@rura.org>
2011-03-27 16:58:22 -0400

    fix to properly handle updates if the project's source URL has changed [resolves #36]

--- dz/tasklib/common_steps.py
+++ dz/tasklib/common_steps.py
@@ -6,6 +6,27 @@ tasks.
 from dz.tasklib import (taskconfig,
                         utils)
 import os
+import shutil
+
+
+def _get_remote_origin_url(repodir):
+    gitdir = os.path.join(repodir, ".git")
+    output, stderr, p = utils.subproc(["git",
+                                       "--git-dir=%s" % gitdir,
+                                       "remote", "show", "origin"],
+                                      redir_stderr_to_stdout=True)
+    if p.returncode != 0:
+        raise utils.InfrastructureException(
+            "Error trying to determine current remote URL for repo: %s"
+            % output)
+
+    prefix = "  Fetch URL: "
+    for line in output.splitlines():
+        if line.startswith(prefix):
+            return line[len(prefix):].strip()
+
+    raise utils.InfrastructureException("Could not find remote origin URL "
+                                        "in git output:\n%s" % output)
 
 
 def checkout_code(zoomdb, opts):
@@ -19,9 +40,11 @@ def checkout_code(zoomdb, opts):
         os.makedirs(d)
 
     if source_code_url.startswith(taskconfig.TEST_REPO_URL_PREFIX):
-        repourl = os.path.join(taskconfig.TEST_REPO_DIR,
-                               source_code_url[len(
-                    taskconfig.TEST_REPO_URL_PREFIX):])
+        suffix = source_code_url[len(taskconfig.TEST_REPO_URL_PREFIX):]
+        suffix = suffix.lstrip("/")
+        assert "/" not in suffix, "Bad test repo path."
+        assert ".." not in suffix, "Bad test repo path."
+        repourl = os.path.join(taskconfig.TEST_REPO_DIR, suffix)
     else:
         repourl = source_code_url
 
@@ -29,10 +52,18 @@ def checkout_code(zoomdb, opts):
     try:
         os.chdir(d)
 
-        if os.path.exists(".git"):
+        if os.path.exists(".git") and \
+               _get_remote_origin_url(d) == source_code_url:
             zoomdb.log("Updating code from repository.")
             cmd = ["git", "pull"]
         else:
+            if os.path.exists(".git"):
+                zoomdb.log("Repository URL has changed, making a fresh clone.")
+                os.chdir(cur_dir)
+                shutil.rmtree(d)
+                os.makedirs(d)
+                os.chdir(d)
+
             zoomdb.log("Cloning your repository.")
             cmd = ["git", "clone", repourl, "."]
 
--- dz/tasklib/tests/unittests/test_common_steps.py
+++ dz/tasklib/tests/unittests/test_common_steps.py
@@ -5,6 +5,8 @@ from dz.tasklib import (taskconfig,
 from dz.tasklib.tests.dztestcase import DZTestCase
 from dz.tasklib.tests.stub_zoomdb import StubZoomDB
 
+import os
+
 
 class CommonStepsTestCase(DZTestCase):
     def setUp(self):
@@ -23,3 +25,38 @@ class CommonStepsTestCase(DZTestCase):
         with self.assertRaises(utils.ProjectConfigurationException):
             self.opts["SRC_URL"] = "fake url"
             common_steps.checkout_code(self.zoomdb, self.opts)
+
+    def test_change_of_repo_url(self):
+        """
+        Test checking out from a changed repo URL.
+        """
+        repos = []
+        for repo_num in range(2):
+            repo_dir = self.makeDir()
+            utils.local("git init %s" % repo_dir)
+            utils.local("touch %s/file_%d" % (repo_dir, repo_num))
+            utils.local("cd %s; git add file_%d; git commit -m 'unittest'" % (
+                repo_dir, repo_num))
+            repos.append(dict(num=repo_num,
+                              path=repo_dir))
+
+        co_dir = self.makeDir()
+        opts = {"CO_DIR": co_dir,
+                "SRC_URL": repos[0]["path"]}
+        common_steps.checkout_code(self.zoomdb, opts)
+        self.assertTrue(os.path.isfile("%s/file_%d" % (co_dir, 0)))
+        self.assertFalse(os.path.isfile("%s/file_%d" % (co_dir, 1)))
+
+        # now change the repo source url
+        opts["SRC_URL"] = repos[1]["path"]
+        common_steps.checkout_code(self.zoomdb, opts)
+        self.assertTrue(os.path.isfile("%s/file_%d" % (co_dir, 1)),
+                        "New repo should be in place.")
+        self.assertFalse(os.path.isfile("%s/file_%d" % (co_dir, 0)),
+                         "Old repo shouldn't still be in place.")
+
+        # and run again to ensure we now do a pull
+        self.zoomdb.logs = []
+        self.assertFalse(("Running git pull...", "i") in self.zoomdb.logs)
+        common_steps.checkout_code(self.zoomdb, opts)
+        self.assertTrue(("Running git pull...", "i") in self.zoomdb.logs)
